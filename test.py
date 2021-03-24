9377b6676b92e977415f4ba748b906ebd1bd8e56
David <david2022es@gmail.com>
2021-03-24 11:56:49 +0100

    Checkpint final

--- test.py
+++ test.py
@@ -2,7 +2,20 @@ import unittest
 from libro import *
 from autor import *
 
+def mas_antiguo(lista, anyo):
+    
+        if anyo < 1900 or anyo > 2021:
+            raise ValueError("El año no es válido")
+
+        res= []
+        for libro in lista:
+            if libro.get_anyo() <= anyo:
+                res.append(libro.get_titulo())
+
+        return res
+
 class Pruebas(unittest.TestCase):
+
     def test_value_error(self):
         lista= [Libro(Autor(1,"Edgar", "Allan Poe"),"Blancanieves1",1932),Libro(Autor(1,"Edgar", "Allan Poe"),"Blancanieves2",1950),Libro(Autor(1,"Edgar", "Allan Poe"),"Blancanieves3",1960),Libro(Autor(1,"Edgar", "Allan Poe"),"Blancanieves4",2020),Libro(Autor(1,"Edgar", "Allan Poe"),"Blancanieves5",2000)]
         self.assertRaisesRegex(ValueError, "El año no es válido", mas_antiguo, lista, 1800)
